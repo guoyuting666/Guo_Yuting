12d329f830bc7b26a57571bb541a116105cdc062
guoyuting666 <769140403@qq.com>
2019-03-11 19:31:18 +0800

    2.3

---
+++ 2.3gytmain.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+
+int main()
+{
+int c,thisIsAVariable,q76354,number,age;
+std::cout <<"Enter an integer ";
+std::cin >> age;
+if(number !=7)
+    std::cout<<"The variable number is not equal to 7\n";
+std::cout<<"This is a C++ program\n";
+std::cout<<"This is a C++\nprogram\n";
+std::cout<<"This\nis\nC++\nprogram\n";
+std::cout<<"This\tis\ta\tC++\tprogram\n";
+return 0;
+}
